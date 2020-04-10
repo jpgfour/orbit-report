de08720a2bb7b4d9e8b99adf695616c4764dc96d
Jeremy <jeremy.grise@gmail.com>
2020-04-09 20:57:25 -0500

    got zebra stripes to work, sort of

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -15,6 +15,8 @@ export class OrbitListComponent implements OnInit {
   ngOnInit() {
   }
 
+  flipFlop: boolean = true;
+
   sort(column: string): void {
     // array.sort modifies the array, sorting the items based on the given compare function
     this.satellites.sort(function(a: Satellite, b: Satellite): number {
@@ -25,6 +27,25 @@ export class OrbitListComponent implements OnInit {
        }
        return 0;
     });
+  }
+
+  stripe(satellite): string {
+    
+    if(satellite.shouldShowWarning()){
+      this.flipFlop = !this.flipFlop;
+      return "#f72c2c";
+      
+    }
+    else if(this.flipFlop) {
+      this.flipFlop = !this.flipFlop;
+      return "aqua";
+  
+    }else{
+      this.flipFlop = !this.flipFlop;
+      return "pink";
+      
+    }
+    this.flipFlop = !this.flipFlop;
   }
 
 }
